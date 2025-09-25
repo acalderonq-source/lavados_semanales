ff7f3a935ecc87f405040f904dd564773deddc23
Alexandro <acalderon.q@tomza.com>
2025-09-25 15:58:27 -0600

    hello.py para diagnóstico

---
+++ hello.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title="Test Render", layout="wide")
+st.title("✅ Render ejecuta Streamlit")
+st.write("Si ves esto, el runtime y el puerto están bien.")
